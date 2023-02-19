d28a0c0fd975a4ea97ab7dada5d14bafcecb23b4
Fiverus <125807326+Fiverus@users.noreply.github.com>
2023-02-19 19:32:59 +0200

    Update main.py
    Vladislavs Fedins 221RDB416 16.grupa

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
 
-
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
-
 
 def find_mismatch(text):
     opening_brackets_stack = []
@@ -24,12 +22,10 @@ def find_mismatch(text):
         return opening_brackets_stack[0].position
     return "Success"
 
-
 def main():
     text = input()
     mismatch = find_mismatch(text)
     print(mismatch)
-
 
 if __name__ == "__main__":
     mode = input()
